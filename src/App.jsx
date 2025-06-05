a2bd9d42292bfac7a5e515c1de93844fbfe06ee6
Sarahi <mtzs19990@gmail.com>
2025-06-05 17:09:30 -0600

    Se modifico el contenido de App y se importo bootstrap en main

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,15 @@ function App() {
 
   return (
     <>
-     <h1>Hola</h1>
+     <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h1 className="mt-2 text-center alert alert-success">
+            hola utsh 
+          </h1>
+        </div>
+      </div>
+     </div>
     </>
   )
 }
